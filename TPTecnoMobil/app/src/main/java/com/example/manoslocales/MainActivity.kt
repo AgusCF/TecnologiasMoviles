687e549b08bcf3c2e8c27fabbe9fbfe8dc08ac1e
AgusCF <acardetti295@alumnos.iua.edu.ar>
2025-05-11 18:45:14 -0300

    .

--- TPTecnoMobil/app/src/main/java/com/example/manoslocales/MainActivity.kt
+++ TPTecnoMobil/app/src/main/java/com/example/manoslocales/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ManosLocalesTheme {
-                RegistroScreen()
+                LoginScreen()
             }
         }
     }
